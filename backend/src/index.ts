a3cb0e476a59c36b98c1a4e551f5a14ee2caa3c5
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-23 07:57:40 -0300

    add typeorm connection at app startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,31 @@
+import 'reflect-metadata'
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import 'reflect-metadata'
+import * as dotenv from 'dotenv'
 import loginRoutes from './routes/login/routes'
+import { createConnection } from 'typeorm'
 
-const app = express()
+dotenv.config()
 
-const PORT = 3002
+createConnection({
+  type: 'postgres',
+  host: process.env.DBHOST,
+  port: Number.parseInt(process.env.DBPORT as string),
+  username: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
+  entities: [__dirname + '/model/*.ts'],
+  synchronize: true
+}).then(() => {
+  console.log('Database connection established!')
+  const app = express()
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(loginRoutes)
+  const PORT = process.env.PORT || 3003
 
-app.listen(PORT, () => console.log('Server running!'))
+  app.use(cors())
+  app.use(bodyParser.json())
+  app.use(loginRoutes)
+
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
