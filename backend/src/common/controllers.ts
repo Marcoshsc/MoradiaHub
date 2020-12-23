f538f7d38f305dbd8b3ba13a2e401521ed9947e9
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-23 09:42:43 -0300

    add error request handler typing

--- backend/src/common/controllers.ts
+++ backend/src/common/controllers.ts
@@ -1,10 +1,18 @@
 import {
   Request as ExpressRequest,
   Response as ExpressResponse,
-  RequestHandler as ExpressRequestHandler
+  RequestHandler as ExpressRequestHandler,
+  ErrorRequestHandler as ExpressErrorRequestHandler
 } from 'express'
 import { ParamsDictionary, Query } from 'express-serve-static-core'
+import { ErrorBody } from './dto'
 
 export type RequestHandler<ResBody, ReqBody> = ExpressRequestHandler<ParamsDictionary, ResBody, ReqBody, Query>
+export type ErrorRequestHandler<ResBody extends ErrorBody, ReqBody> = ExpressErrorRequestHandler<
+  ParamsDictionary,
+  ResBody,
+  ReqBody,
+  Query
+>
 export type Request<ResBody, ReqBody> = ExpressRequest<ParamsDictionary, ResBody, ReqBody, Query>
 export type Response<ResBody> = ExpressResponse<ResBody>
