c81a448805ef37b99f36635527f32a5a69dc96b9
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-23 07:57:46 -0300

    create user entity

---
+++ backend/src/model/user.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity({
+  name: 'user'
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    length: 100
+  })
+  name: string
+
+  @Column({
+    length: 15
+  })
+  phone: string
+
+  @Column({
+    length: 100,
+    unique: true
+  })
+  email: string
+
+  @Column()
+  password: string
+
+  @Column({
+    type: 'timestamp',
+    default: (): string => 'now()'
+  })
+  createdAt?: Date
+
+  @Column({
+    type: 'timestamp',
+    default: (): string => 'now()'
+  })
+  updatedAt?: Date
+}
