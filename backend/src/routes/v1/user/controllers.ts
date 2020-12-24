80d29f53b9db6ccc8678c934dda690c7a59415cc
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-24 17:09:39 -0300

    create controllers for each new created service

--- backend/src/routes/v1/user/controllers.ts
+++ backend/src/routes/v1/user/controllers.ts
@@ -1,6 +1,15 @@
 import { RequestHandler } from '../../../common/controllers'
 import { UserDTO } from '../../../dto/user'
+import { insertUser, getAllUsers } from '../../../services/user'
 
 export const insert: RequestHandler<UserDTO, UserDTO> = (req, res, next) => {
-  res.send(req.body)
+  insertUser(req.body)
+    .then((user) => res.status(200).json(user))
+    .catch((err) => next(err))
+}
+
+export const getAll: RequestHandler<UserDTO[], void> = (req, res, next) => {
+  getAllUsers()
+    .then((users) => res.status(200).json(users))
+    .catch((err) => next(err))
 }
