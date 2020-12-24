c32c643a9bfcd0b9381b15e1ce4a386a13e4e3ce
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-24 18:29:41 -0300

    add authentication handler

--- backend/src/express/globalHandlers.ts
+++ backend/src/express/globalHandlers.ts
@@ -1,8 +1,31 @@
 import { ErrorRequestHandler, RequestHandler } from '../common/controllers'
 import { ErrorBody } from '../common/dto'
+import LoginEndpoints from '../routes/v1/login/endpoints'
+import { generateJwt, validateJwt } from '../services/authentication'
+import { getVersionatedUrl } from '../utils/urlUtils'
 
 export interface NotFoundPage extends ErrorBody {
   endpoint: string
+}
+
+export const authenticationHandler: RequestHandler<ErrorBody, void> = (req, res, next) => {
+  if (req.url === getVersionatedUrl(LoginEndpoints.BASE_URL)) {
+    next()
+    return
+  }
+
+  if (!req.headers.authorization) {
+    res.status(401).json({
+      error: 'No authorization data was provided.'
+    })
+    return
+  }
+  const token = req.headers.authorization.replace('Bearer ', '')
+  const userId = validateJwt(token)
+  res.locals.jwtPayload = userId
+  const newToken = generateJwt(userId)
+  res.setHeader('token', newToken)
+  next()
 }
 
 export const notFoundHandler: RequestHandler<NotFoundPage, void> = (req, res) => {
