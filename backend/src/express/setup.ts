83861616eaee6412e1068554aa90bb124d517087
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-24 18:29:54 -0300

    incorporate authentication handler

--- backend/src/express/setup.ts
+++ backend/src/express/setup.ts
@@ -5,13 +5,14 @@ import bodyParser from 'body-parser'
 import loginRoutes from '../routes/v1/login/routes'
 import userRoutes from '../routes/v1/user/routes'
 import { getVersionatedUrl } from '../utils/urlUtils'
-import { internalServerErrorHandler, notFoundHandler } from './globalHandlers'
+import { authenticationHandler, internalServerErrorHandler, notFoundHandler } from './globalHandlers'
 import { UserEndpoints } from '../routes/v1/user/endpoints'
 import LoginEndpoints from '../routes/v1/login/endpoints'
 
 export const configureExpress = (app: Express): void => {
   app.use(cors())
   app.use(bodyParser.json())
+  app.use(authenticationHandler)
 }
 
 export const setupExpressRoutes = (app: Express): void => {
