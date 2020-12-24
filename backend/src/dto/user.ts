64b0cc84a83b4b0322dd6ece1bb1c42632be7e21
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-24 17:07:40 -0300

    add password field to user dto and create factory methods (user -> dto and dto->user)

--- backend/src/dto/user.ts
+++ backend/src/dto/user.ts
@@ -1,8 +1,28 @@
+import { User } from '../model/user'
+import { getFormattedTimeStamp } from '../utils/date'
+
 export interface UserDTO {
   id: number
   email: string
   name: string
   phone: string
+  password?: string
   createdAt?: string
   updatedAt?: string
 }
+
+export const getDTOFromUser = (entity: User): UserDTO => ({
+  ...entity,
+  id: entity.id as number,
+  createdAt: getFormattedTimeStamp(entity.createdAt as Date),
+  updatedAt: getFormattedTimeStamp(entity.updatedAt as Date),
+  password: undefined
+})
+
+export const getUserFromDTO = (dto: UserDTO): User => ({
+  ...dto,
+  id: undefined,
+  password: dto.password as string,
+  createdAt: undefined,
+  updatedAt: undefined
+})
