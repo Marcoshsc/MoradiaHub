c8a81bfbf1a9d7705fc101949fdad0e0db75b40f
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-25 10:32:23 -0300

    use new error handlers on express

--- backend/src/express/setup.ts
+++ backend/src/express/setup.ts
@@ -5,7 +5,13 @@ import bodyParser from 'body-parser'
 import loginRoutes from '../routes/v1/login/routes'
 import userRoutes from '../routes/v1/user/routes'
 import { getVersionatedUrl } from '../utils/urlUtils'
-import { authenticationHandler, internalServerErrorHandler, notFoundHandler } from './globalHandlers'
+import {
+  authenticationHandler,
+  badRequestErrorHandler,
+  internalServerErrorHandler,
+  notFoundHandler,
+  unauthorizedErrorHandler
+} from './globalHandlers'
 import { UserEndpoints } from '../routes/v1/user/endpoints'
 import LoginEndpoints from '../routes/v1/login/endpoints'
 
@@ -21,6 +27,8 @@ export const setupExpressRoutes = (app: Express): void => {
 }
 
 export const setupHandlers = (app: Express): void => {
+  app.use(badRequestErrorHandler)
+  app.use(unauthorizedErrorHandler)
   app.use(internalServerErrorHandler)
   app.use(notFoundHandler)
 }
