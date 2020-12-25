06a956485379b4467c1d8628f08e9583d0aa9ebe
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-25 09:56:38 -0300

    refactor user insert method to send a verification email after insertion

--- backend/src/routes/v1/user/controllers.ts
+++ backend/src/routes/v1/user/controllers.ts
@@ -1,10 +1,22 @@
 import { RequestHandler } from '../../../common/controllers'
 import { UserDTO } from '../../../dto/user'
+import { MailTemplates } from '../../../mail/template'
+import { sendSimpleMail } from '../../../services/mailService'
 import { insertUser, getAllUsers } from '../../../services/user'
+import { getFullUrl } from '../../../utils/urlUtils'
+import { UserEndpoints } from './endpoints'
 
 export const insert: RequestHandler<UserDTO, UserDTO> = (req, res, next) => {
   insertUser(req.body)
-    .then((user) => res.status(201).json(user))
+    .then(async (dto) => {
+      const { user, validationEndpoint } = dto
+      res.status(201).json(user)
+      const replacements = {
+        name: user.name,
+        link: getFullUrl(`${UserEndpoints.BASE_URL}/${user.id}/validatemail/${validationEndpoint}`)
+      }
+      await sendSimpleMail(MailTemplates.EMAIL_VALIDATION, replacements, user.email, 'MoradiaHub: Validação de Email.')
+    })
     .catch((err) => next(err))
 }
 
--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,7 +1,12 @@
 import { getRepository } from 'typeorm'
-import { getDTOFromUser, getUserFromDTO, UserDTO } from '../dto/user'
+import { getDTOFromUser, getUserFromDTO, UserDTO, UserWithAdditionalInfoDTO } from '../dto/user'
 import { User } from '../model/user'
+import randomstring from 'randomstring'
 import bcrypt from 'bcrypt'
+import { sendSimpleMail } from './mailService'
+import { MailTemplates } from '../mail/template'
+import { getFullUrl } from '../utils/urlUtils'
+import { UserEndpoints } from '../routes/v1/user/endpoints'
 
 export const getAllUsers = async (): Promise<UserDTO[]> => {
   const userRepository = getRepository(User)
@@ -21,17 +26,20 @@ export const loginUser = async (email: string, password: string): Promise<UserDT
   return getDTOFromUser(user)
 }
 
-export const insertUser = async (dto: UserDTO): Promise<UserDTO> => {
+export const insertUser = async (dto: UserDTO): Promise<UserWithAdditionalInfoDTO> => {
   const user = getUserFromDTO(dto)
 
   const password = user.password
   const salt = await bcrypt.genSalt()
   const hashedPassword = await bcrypt.hash(password, salt)
 
+  const validationEndpoint = randomstring.generate(20)
+  user.validationEndpoint = validationEndpoint
+
   user.password = hashedPassword
 
   const userRepository = getRepository(User)
   const savedUser = await userRepository.save(user)
-  console.log(savedUser)
-  return getDTOFromUser(savedUser)
+
+  return { user: getDTOFromUser(savedUser), validationEndpoint: savedUser.validationEndpoint as string }
 }
