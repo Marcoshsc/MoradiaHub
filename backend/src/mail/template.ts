98bfbc159fa7624207050019b8ea35494c027390
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-25 09:53:45 -0300

    add enum with all Mail templates

--- backend/src/mail/template.ts
+++ backend/src/mail/template.ts
@@ -2,6 +2,10 @@ import * as path from 'path'
 import * as fs from 'fs'
 import * as handlebars from 'handlebars'
 
+export enum MailTemplates {
+  EMAIL_VALIDATION = 'emailValidation'
+}
+
 export const getTemplate = (name: string, replacements: unknown): string => {
   const filePath = path.join(__dirname, `../../templates/${name}.html`)
   const source = fs.readFileSync(filePath, 'utf-8').toString()
