57cf1ea3a908d1914b902dfb86382f13d7598655
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-25 15:04:36 -0300

    comment authentication service methods

--- backend/src/services/authentication.ts
+++ backend/src/services/authentication.ts
@@ -1,5 +1,10 @@
 import * as jwt from 'jsonwebtoken'
 
+/**
+ * Validate a JWT token and return the user info present on it.
+ * @param token the token to be validated
+ * @returns the user ID present on the key.
+ */
 export const validateJwt = (token: string): number => {
   const secret = process.env.JWTSECRET as string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -7,6 +12,11 @@ export const validateJwt = (token: string): number => {
   return userId
 }
 
+/**
+ * Generate a JWT key for the given user info.
+ * @param userId the user ID to generate the key
+ * @returns a JWT key for the given user info.
+ */
 export const generateJwt = (userId: number): string => {
   const payload = { userId }
   const secret = process.env.JWTSECRET as string
