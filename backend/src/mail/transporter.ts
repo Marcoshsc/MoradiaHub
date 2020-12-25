babc29cd55a02827896a18180490ebe332d607f0
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-25 01:13:00 -0300

    configure nodemailer transporter

---
+++ backend/src/mail/transporter.ts
@@ -0,0 +1,13 @@
+import * as nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTPHOST as string,
+  port: Number.parseInt(process.env.SMTPPORT as string),
+  auth: {
+    user: process.env.SMTPUSER,
+    pass: process.env.SMTPPASSWORD
+  },
+  tls: { rejectUnauthorized: false }
+})
+
+export default transporter
