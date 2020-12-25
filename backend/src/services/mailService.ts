41396ab182fb53cad0192553f7e128aa5d8e0d44
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-25 09:55:41 -0300

    incorporate new enums to mail service

--- backend/src/services/mailService.ts
+++ backend/src/services/mailService.ts
@@ -1,15 +1,15 @@
+import { getMailFrom } from '../mail/global'
 import { getTemplate } from '../mail/template'
 import transporter from '../mail/transporter'
 
 export const sendSimpleMail = async (
   template: string,
   replacements: unknown,
-  from: string,
   to: string,
   subject: string
 ): Promise<void> => {
   await transporter.sendMail({
-    from: from,
+    from: getMailFrom(),
     to: to,
     subject: subject,
     html: getTemplate(template, replacements)
