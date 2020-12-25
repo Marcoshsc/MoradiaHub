daf80a12045a1ec94cae4d240ac301e53901cb13
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-25 09:50:26 -0300

    add utility method to get full webservice url

--- backend/src/utils/urlUtils.ts
+++ backend/src/utils/urlUtils.ts
@@ -3,3 +3,7 @@ import { ApiVersions, ApiVersionsKey } from '../express/api'
 export const getVersionatedUrl = (relative: string): string => {
   return ApiVersions[process.env.APIVERSION as ApiVersionsKey] + relative
 }
+
+export const getFullUrl = (relative: string): string => {
+  return process.env.WEBSERVICEBASEURL + relative
+}
