69d0802642d7c51ca84dc95c3ba34ee153a31068
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-25 15:04:43 -0300

    comment mail service methods

--- backend/src/services/mailService.ts
+++ backend/src/services/mailService.ts
@@ -2,6 +2,13 @@ import { getMailFrom } from '../mail/global'
 import { getTemplate } from '../mail/template'
 import transporter from '../mail/transporter'
 
+/**
+ * Send a simple mail, with the given template, using the given replacements.
+ * @param template the mail template
+ * @param replacements the replacements on the template
+ * @param to who will receive the mail
+ * @param subject the mail subject
+ */
 export const sendSimpleMail = async (
   template: string,
   replacements: unknown,
