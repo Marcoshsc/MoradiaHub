647aa0dc75003bea5352ecef7cf1da876d114157
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-26 16:09:14 -0300

    finish config for github pages and any other deployment provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom'
 import MainComponent from './pages/main'
 
 const App: React.FunctionComponent = () => {
-  return (
-    <Router basename="/">
+  return process.env.REACT_APP_DEPLOY_ENVIRONMENT && process.env.REACT_APP_DEPLOY_ENVIRONMENT === 'github' ? (
+    <HashRouter>
+      <MainComponent />
+    </HashRouter>
+  ) : (
+    <Router>
       <MainComponent />
     </Router>
   )
