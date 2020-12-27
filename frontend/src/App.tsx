5747d3af65f9f45fbc6b91f94f9d6e434dd913b0
Marcoshsc <marcoshscunha@hotmail.com>
2020-12-27 09:38:58 -0300

    update production/development switching environment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable indent */
 import React from 'react'
 import { BrowserRouter as Router, HashRouter } from 'react-router-dom'
 import MainComponent from './pages/main'
 
 const App: React.FunctionComponent = () => {
-  return process.env.REACT_APP_DEPLOY_ENVIRONMENT && process.env.REACT_APP_DEPLOY_ENVIRONMENT === 'github' ? (
+  return process.env.NODE_ENV === 'production' &&
+    process.env.REACT_APP_DEPLOY_ENVIRONMENT &&
+    process.env.REACT_APP_DEPLOY_ENVIRONMENT === 'github' ? (
     <HashRouter>
       <MainComponent />
     </HashRouter>
